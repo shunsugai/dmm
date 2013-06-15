f24c24ad5afc0f46479e99ceb36b594003ddced5
Shun Sugai <sugaishun@gmail.com>
2013-06-16 00:20:15 +0900

    Fix for test failure

--- spec/dmm/item_spec.rb
+++ spec/dmm/item_spec.rb
@@ -5,7 +5,7 @@ describe Dmm::Item do
   context 'with valid response' do
     before(:all) do
       time = Time.now.to_s.gsub(/\s/, '+')
-      request_url = 'http://affiliate-api.dmm.com/?api_id=API_ID&affiliate_id=AFFILIATE_ID&operation=ItemList&version=2.00&timestamp='+ time +'&site=DMM.co.jp&keyword=%B5%F0%C6%FD'
+      request_url = 'http://affiliate-api.dmm.com/?api_id=API_ID&affiliate_id=AFFILIATE_ID&operation=ItemList&version=2.00&timestamp=' + time + '&site=DMM.co.jp&keyword=%B5%F0%C6%FD'
       stub_request(:get, request_url).to_return(:status => 200, :body => fixture('sample2.xml'))
       client = Dmm.new(:api_id => 'API_ID', :affiliate_id => 'AFFILIATE_ID')
       client.instance_variable_set(:@time, time)
